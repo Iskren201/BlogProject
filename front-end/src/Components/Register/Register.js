56178d6d92d83d2a4d73eb0ae7b3865ecea05f13
Iskren201 <iskren201@gmail.com>
2024-02-29 21:08:03 +0200

    adding Register and DB

--- front-end/src/Components/Register/Register.js
+++ front-end/src/Components/Register/Register.js
@@ -1,36 +1,133 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
+import axios from "axios";
+import { toast, ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 const Register = () => {
-    return <div>
-        <div class="h-screen bg-indigo-100 flex justify-center items-center">
-            <div class="lg:w-2/5 md:w-1/2 w-2/3">
-                <form class="bg-white p-10 rounded-lg shadow-lg min-w-full">
-                    <h1 class="text-center text-2xl mb-6 text-gray-600 font-bold font-sans">Become a Writer</h1>
-                    <div>
-                        <label class="text-gray-800 font-semibold block my-3 text-md" for="username">Username</label>
-                        <input class="w-full bg-gray-100 px-4 py-2 rounded-lg focus:outline-none" type="text" name="username" id="username" placeholder="username" />
-                    </div>
-                    <div>
-                        <label class="text-gray-800 font-semibold block my-3 text-md" for="email">Email</label>
-                        <input class="w-full bg-gray-100 px-4 py-2 rounded-lg focus:outline-none" type="text" name="email" id="email" placeholder="@email" />
-                    </div>
-                    <div>
-                        <label class="text-gray-800 font-semibold block my-3 text-md" for="password">Password</label>
-                        <input class="w-full bg-gray-100 px-4 py-2 rounded-lg focus:outline-none" type="text" name="password" id="password" placeholder="password" />
-                    </div>
-                    <div>
-                        <label class="text-gray-800 font-semibold block my-3 text-md" for="confirm">Confirm password</label>
-                        <input class="w-full bg-gray-100 px-4 py-2 rounded-lg focus:outline-none" type="text" name="confirm" id="confirm" placeholder="confirm password" />
-                    </div>
-                    <button type="submit" class="w-full mt-6 bg-indigo-600 rounded-lg px-4 py-2 text-lg text-white tracking-wide font-semibold font-sans">Register</button>
-                    <Link to='/login' className="w-full mt-6 mb-3 rounded-lg px-4 py-2 text-gray-600 font-semibold flex justify-center items-center hover:bg-gray-100 hover:text-gray-800 transition-colors duration-300">
-                        Already have an account? Click here to log in!
-                    </Link>
-                </form>
+  const [formData, setFormData] = useState({
+    username: "",
+    email: "",
+    password: "",
+  });
+
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    if (!validateForm()) {
+      return;
+    }
+    try {
+      const { username, email, password } = formData;
+      await axios.post("http://localhost:3333/register", {
+        username,
+        email,
+        password,
+      });
+      toast.success(`${email} registered successfully`);
+    } catch (error) {
+      if (error.response && error.response.status === 400) {
+        toast.error(error.response.data.message);
+      } else {
+        toast.error("An error occurred. Please try again later.");
+      }
+    }
+  };
+
+  const validateForm = () => {
+    const { username, email, password } = formData;
+    if (!username || !email || !password) {
+      toast.error("All fields are required");
+      return false;
+    }
+    // You can add more specific validation checks for email and password if needed
+    return true;
+  };
+
+  return (
+    <div>
+      <ToastContainer />
+      <div className="h-screen bg-indigo-100 flex justify-center items-center">
+        <div className="lg:w-2/5 md:w-1/2 w-2/3">
+          <form
+            className="bg-white p-10 rounded-lg shadow-lg min-w-full"
+            onSubmit={handleSubmit}
+          >
+            <h1 className="text-center text-2xl mb-6 text-gray-600 font-bold font-sans">
+              Become a Writer
+            </h1>
+
+            <div>
+              <label
+                className="text-gray-800 font-semibold block my-3 text-md"
+                htmlFor="username"
+              >
+                Username
+              </label>
+              <input
+                className="w-full bg-gray-100 px-4 py-2 rounded-lg focus:outline-none"
+                type="text"
+                name="username"
+                id="username"
+                placeholder="username"
+                value={formData.username}
+                onChange={handleChange}
+              />
             </div>
+            <div>
+              <label
+                className="text-gray-800 font-semibold block my-3 text-md"
+                htmlFor="email"
+              >
+                Email
+              </label>
+              <input
+                className="w-full bg-gray-100 px-4 py-2 rounded-lg focus:outline-none"
+                type="email"
+                name="email"
+                id="email"
+                placeholder="email@example.com"
+                value={formData.email}
+                onChange={handleChange}
+              />
+            </div>
+            <div>
+              <label
+                className="text-gray-800 font-semibold block my-3 text-md"
+                htmlFor="password"
+              >
+                Password
+              </label>
+              <input
+                className="w-full bg-gray-100 px-4 py-2 rounded-lg focus:outline-none"
+                type="password"
+                name="password"
+                id="password"
+                placeholder="password"
+                value={formData.password}
+                onChange={handleChange}
+              />
+            </div>
+            <button
+              type="submit"
+              className="w-full mt-6 bg-indigo-600 rounded-lg px-4 py-2 text-lg text-white tracking-wide font-semibold font-sans"
+            >
+              Register
+            </button>
+            <Link
+              to="/login"
+              className="w-full mt-6 mb-3 rounded-lg px-4 py-2 text-gray-600 font-semibold flex justify-center items-center hover:bg-gray-100 hover:text-gray-800 transition-colors duration-300"
+            >
+              Already have an account? Click here to log in!
+            </Link>
+          </form>
         </div>
+      </div>
     </div>
+  );
 };
 
 export default Register;
